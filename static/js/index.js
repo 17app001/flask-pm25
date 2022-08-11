a87b746bd9fcd3ed531e7e80870ac385c2a19dcc
jerrychen <jerry@gmail.com>
2022-08-11 19:52:38 +0800

    增加靜態檔案跟jquery引用

---
+++ static/js/index.js
@@ -0,0 +1,25 @@
+showTime();
+
+function showTime() {
+    $(".date").text(getTime());
+    setTimeout(() => {
+        showTime();
+    }, 1000);
+}
+
+
+function getTime(fullTime = true) {
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let date = now.getDate();
+    let hours = now.getHours();
+    let minutes = String(now.getMinutes()).padStart(2, "0");
+    let seconds = String(now.getSeconds()).padStart(2, "0");
+
+    if (fullTime) {
+        return `${year}/${month}/${date} ${hours}:${minutes}:${seconds}`;
+    }
+
+    return `${year}/${month}/${date}`;
+}
